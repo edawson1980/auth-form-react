5bf68367f861e6b1209f59a6c393051f8e23d1c2
Erin <erinedawson@gmail.com>
2019-09-30 15:59:36 -0600

    add dummy render method

--- app.js
+++ app.js
@@ -18,4 +18,20 @@ authorize(e) {
     authorized: auth
   });
 }
+render(){
+  return(
+    <div id="authorization">
+      <h1>Contact</h1>
+      <ul>
+        <li>
+          client@example.com
+        </li>
+        <li>
+          555.555.5555
+        </li>
+      </ul>
+    </div>
+  );
+}
+
 };
